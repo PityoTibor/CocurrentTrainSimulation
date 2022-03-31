ef92f29725f0446bf2ef447b247da32e7da79d6b
PityoTibor <87979998+PityoTibor@users.noreply.github.com>
2022-03-31 17:37:08 +0200

    Dijsktra

--- ConcurrentTrainSimulation/Edge.cs
+++ ConcurrentTrainSimulation/Edge.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConcurrentTrainSimulation
 {
     public class Stations
     {
-        int nodes = 21;
+        int nodes = 22;
         public Edge[,] matrix;
         public void InitMatrix(int num)
         {
@@ -18,22 +19,22 @@ namespace ConcurrentTrainSimulation
 
         public void CreateStations()
         {
-            AddEdges(new Edge(1, 0, 1, 1.0));
-            AddEdges(new Edge(2, 0, 3, 2.0));
-            AddEdges(new Edge(3, 0, 4, 4.0));
+            AddEdges(new Edge(0, 0, 1, 1.0));
+            AddEdges(new Edge(1, 0, 3, 2.0));
+            AddEdges(new Edge(2, 0, 4, 4.0));
 
-            AddEdges(new Edge(4, 1, 0, 1.0));
-            AddEdges(new Edge(5, 1, 2, 9.0));
-            AddEdges(new Edge(6, 1, 3, 2.0));
+            AddEdges(new Edge(3, 1, 0, 1.0));
+            AddEdges(new Edge(4, 1, 2, 9.0));
+            AddEdges(new Edge(5, 1, 3, 2.0));
 
-            AddEdges(new Edge(7, 2, 1, 9.0));
-            AddEdges(new Edge(8, 2, 3, 5.0));
-            AddEdges(new Edge(9, 2, 5, 5.0));
+            AddEdges(new Edge(6, 2, 1, 9.0));
+            AddEdges(new Edge(7, 2, 3, 5.0));
+            AddEdges(new Edge(8, 2, 5, 5.0));
 
-            AddEdges(new Edge(10, 3, 0, 2.0));
-            AddEdges(new Edge(11, 3, 1, 2.0));
-            AddEdges(new Edge(12, 3, 2, 5.0));
-            AddEdges(new Edge(11, 3, 5, 3.0));
+            AddEdges(new Edge(9, 3, 0, 2.0));
+            AddEdges(new Edge(10, 3, 1, 2.0));
+            AddEdges(new Edge(11, 3, 2, 5.0));
+            AddEdges(new Edge(12, 3, 5, 3.0));
 
             AddEdges(new Edge(13, 4, 0, 4.0));
             AddEdges(new Edge(14, 4, 7, 3.0));
@@ -49,39 +50,60 @@ namespace ConcurrentTrainSimulation
             AddEdges(new Edge(21, 7, 6, 2.0));
         }
 
-        public Dictionary<int, Tuple<double, Edge>> DijsktraShortestRoute()
+        public Dictionary<int, Tuple<double, Edge, List<Edge>>> DijsktraShortestRoute()
         {
-            Dictionary<int, Tuple<double, Edge>> re = new Dictionary<int, Tuple<double, Edge>>();
+            Dictionary<int, Tuple<double, Edge, List<Edge>>> LE = new Dictionary<int, Tuple<double, Edge, List<Edge>>>();
 
-            Queue<Tuple<Edge, int>> S = new Queue<Tuple<Edge, int>>();
+            List<Tuple<Edge, double>> S = new List<Tuple<Edge, double>>();
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
                     if (matrix[i,j] != null)
                     {
-                        S.Enqueue(new Tuple<Edge, int>(matrix[i,j], int.MaxValue));
+                        S.Add(new Tuple<Edge, double>(matrix[i,j], double.MaxValue));
                     }
                 }
             }
-            ;
 
             for (int i = 0; i < nodes; i++)
             {
-                re.Add(i+1, new Tuple<double, Edge>(double.NaN, null));
+                LE.Add(i, new Tuple<double, Edge, List<Edge>>(double.MaxValue, null, null));
             }
-            ;
-
+            
             Edge start = GetStartEdge();
-            re.Add(start.Id, new Tuple<double, Edge>(0, null));
-
-            GetMinValue(S);
+            LE[start.Id] = new Tuple<double, Edge, List<Edge>>(0, null, new List<Edge>());
+            S[start.Id] = new Tuple<Edge, double>(start, 0);
+            
             while (S.Count != 0)
             {
+                var u = GetMinValue(S);
+                var allNeighbour = Neighbours(u);
+                for (int i = 0; i < allNeighbour.Count; i++)
+                {
+                    Edge aktualNeighbour = allNeighbour[i];
 
+                    int uId = SearchEdge(LE, u);
+                    int aktualNeighbourId = SearchEdge(LE, aktualNeighbour);
+
+                    if (LE[u.Id].Item1 + aktualNeighbour.Value < LE[aktualNeighbour.Id].Item1)
+                    {
+                        var list = LE[u.Id].Item3;
+                        list.AddRange(list);
+                        LE[u.Id] = new Tuple<double, Edge, List<Edge>>(LE[u.Id].Item1 + aktualNeighbour.Value, u, list);
+                        for (int j = 0; j < S.Count; j++)
+                        {
+                            if (S[j].Item1 == aktualNeighbour)
+                            {
+                                S[j] = new Tuple<Edge, double>(aktualNeighbour, LE[u.Id].Item1 + aktualNeighbour.Value);
+                            }
+                        }
+                    }
+                }
+                ;
             }
 
-            return re;
+            return LE;
         }
 
         public void VisitAllEdge()
@@ -109,10 +131,38 @@ namespace ConcurrentTrainSimulation
             }
         }
 
-        private void GetMinValue(Queue<Tuple<Edge, int>> S)
+        private int SearchEdge(Dictionary<int, Tuple<double, Edge, List<Edge>>> LE, Edge e)
         {
-            var asd = S.Select(x => x.Item2).Min();
-            ;
+            for (int i = 0; i < LE.Count; i++)
+            {
+                if (LE[i].Item2 == e)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        private Edge GetMinValue(List<Tuple<Edge, double>> S)
+        {
+            double min = S[0].Item2;
+            Edge e = null;
+            foreach (var queue in S)
+            {
+                if (queue.Item2 < min)
+                {
+                    e = queue.Item1;
+                    //itt ki is lehet törölni
+                }
+            }
+            for (int i = 0; i < S.Count; i++)
+            {
+                if (S[i].Item1 == e)
+                {
+                    S.Remove(S[i]);
+                }
+            }
+            return e;
         }
 
 
